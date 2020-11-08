e91afa902a89cb20dae9069b52d6d1a91aa14e2f
castle-sky <44106966+castle-sky@users.noreply.github.com>
2020-11-08 23:03:20 +0800

    Add files via upload

--- castle-sky/webGame/fingerPainting/main.js
+++ castle-sky/webGame/fingerPainting/main.js
@@ -5,7 +5,9 @@ window.global = {fingerMode: 'single',
 				 	single: '单指模式',
 				 	double: '双指模式',
 				 	multiple: '多指模式'
-				 }};
+				 },
+				 lineWidth: 5
+				};
 
 
 //功能1：全屏模式
@@ -87,6 +89,7 @@ window.addEventListener('load', function () {
 		colorOptions.style.display = colorOptionsDispaly;
 		colorIcon.innerHTML = icon;
 	}, false);
+
 	colorOptions.addEventListener('click', function (e) {
 		if (e.target.nodeName == 'SPAN') {
 			var color = e.target.dataset.color;
@@ -108,6 +111,77 @@ window.addEventListener('load', function () {
 	}
 }, false);
 
+//功能4：切换线宽
+window.addEventListener('load', function () {
+	var currentLineWidth = document.querySelector('span.currentLineWidth');
+	var toggleLineWidth = document.querySelector('span.toggleLineWidth');
+	var lineWidthIcon = document.querySelector('span.lineWidthIcon');
+	var lineWidthOptions = document.querySelector('div.lineWidthOptions');
+	var moreLineWidth = document.querySelector('span.moreLineWidth');
+
+	lineWidthOptions.style.display = 'none';
+
+	toggleLineWidth.addEventListener('click', function () {
+		var lineWidthOptionsDispaly = lineWidthOptions.style.display == 'none' ? 'inline-block' : 'none';
+		var icon = lineWidthOptions.style.display == 'none' ? '<<' : '>>';
+
+		lineWidthOptions.style.display = lineWidthOptionsDispaly;
+		lineWidthIcon.innerHTML = icon;
+	}, false);
+
+	lineWidthOptions.addEventListener('click', function (e) {
+		if (e.target.nodeName == 'SPAN') {
+			var lineWidth = e.target.dataset.lineWidth;
+			currentLineWidth.dataset.lineWidth = lineWidth;
+			global.lineWidth = lineWidth;
+			setLineWidth('span.currentLineWidth');
+		}
+	}, false);
+
+	setLineWidth('span.currentLineWidth');
+
+	function setLineWidth(selector) {
+		var results = document.querySelectorAll(selector);
+		for (let item of results) {
+			item.innerHTML = item.dataset.lineWidth;
+		}
+	}
+
+	moreLineWidth.addEventListener('click', function (e) {
+		e.stopPropagation();
+
+		var result = prompt('请输入新的规格值（正数）');
+		console.log(Number(result));
+		if (result <= 0) {
+			alert('输入有误！！！！');
+		} else {
+			currentLineWidth.dataset.lineWidth = Number(result);
+			global.lineWidth = Number(result);
+			setLineWidth('span.currentLineWidth');
+		}
+	}, false);
+
+}, false);
+
+//功能6：设置绘图区大小
+window.addEventListener('load', function () {
+	var confirm = document.querySelector('input#confirm');
+	var warning = document.querySelector('span.warning');
+	var canvas = document.querySelector('canvas');
+	var canvasWidth = document.querySelector('input#canvasWidth');
+	var canvasHeight = document.querySelector('input#canvasHeight');
+
+	confirm.addEventListener('click', function(e) {
+		if (canvasWidth.value > 0 && canvasHeight.value > 0) {
+			canvas.width = canvasWidth.value;
+			canvas.height = canvasHeight.value;
+			warning.style.display = 'none';
+		} else {
+			warning.style.display = 'block';
+		}
+	}, false)
+}, false);
+
 //canvas绘图代码
 window.addEventListener('load', function () {
 	var clearCanvas = document.querySelector('span.clearCanvas');
@@ -124,6 +198,8 @@ window.addEventListener('load', function () {
 	canvas.addEventListener('touchend', endHandler, false);
 
 	function startHandler(e) {
+		//当处于“禁用手指”模式时，恢复touch的默认行为，
+		//因此可以在“禁用手指”模式滚动、缩放页面
 		if (!(global.fingerMode == 'none')) {
 			e.preventDefault();			
 		}
@@ -161,13 +237,15 @@ window.addEventListener('load', function () {
 
 		for (let i = 0; i < length; i++) {
 			ctx.beginPath();
-			ctx.arc(touches[i].pageX - canvas.offsetLeft, touches[i].pageY - canvas.offsetTop, 3, 0, 2 * Math.PI, false);
+			ctx.arc(touches[i].pageX - canvas.offsetLeft, touches[i].pageY - canvas.offsetTop, global.lineWidth / 2, 0, 2 * Math.PI, false);
 			ctx.fillStyle = global.color;
 			ctx.fill();
 		}
 	}
 
 	function moveHandler(e) {
+		//当处于“禁用手指”模式时，恢复touch的默认行为，
+		//因此可以在“禁用手指”模式滚动、缩放页面
 		if (!(global.fingerMode == 'none')) {
 			e.preventDefault();			
 		}
@@ -182,7 +260,7 @@ window.addEventListener('load', function () {
 				ctx.moveTo(ongoingTouches[idx].pageX - canvas.offsetLeft, ongoingTouches[idx].pageY - canvas.offsetTop);
 				ctx.lineTo(touches[i].pageX - canvas.offsetLeft, touches[i].pageY - canvas.offsetTop);
 				ctx.strokeStyle = global.color;
-				ctx.lineWidth = 5;
+				ctx.lineWidth = global.lineWidth;
 				ctx.lineCap = 'round';
 				ctx.stroke();
 
@@ -192,6 +270,8 @@ window.addEventListener('load', function () {
 	}
 
 	function endHandler(e) {
+		//当处于“禁用手指”模式时，恢复touch的默认行为，
+		//因此可以在“禁用手指”模式滚动、缩放页面
 		if (!(global.fingerMode == 'none')) {
 			e.preventDefault();			
 		}
@@ -203,7 +283,7 @@ window.addEventListener('load', function () {
 
 			if (idx > -1) {
 				ctx.beginPath();
-				ctx.arc(touches[i].pageX - canvas.offsetLeft, touches[i].pageY - canvas.offsetTop, 3, 0, 2 * Math.PI, false);
+				ctx.arc(touches[i].pageX - canvas.offsetLeft, touches[i].pageY - canvas.offsetTop, global.lineWidth / 2, 0, 2 * Math.PI, false);
 				ctx.fillStyle = global.color;
 				ctx.fill();
 
