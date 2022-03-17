0338b1b2b6db47e3b436c23784df0042976cd2fc
castle-sky <familyboat@163.com>
2022-03-17 16:08:25 +0800

    [fix] 显示浏览器对 barcode 的支持情况

--- web-api/barcode-detection-api/main.js
+++ web-api/barcode-detection-api/main.js
@@ -14,6 +14,27 @@
     supportDetectElem.textContent = '支持 barcode';
   }
 
+  const formatList = [
+    'aztec',
+    'code_128',
+    'code_39',
+    'code_93',
+    'data_matrix',
+    'ean_13',
+    'ean_8',
+    'itf',
+    'pdf417',
+    'qr_code',
+    'upc_e',
+  ]
   const supportedFormats = await BarcodeDetector.getSupportedFormats();
-  supportFormatsElem.innerHTML = supportedFormats.map(format => `<li>${format}</li>`).join('\n');
+ 
+  supportFormatsElem.innerHTML = formatList.map(format => {
+    const html = '<li>是否支持${format}(result)</li>';
+    if (supportedFormats.length && supportedFormats.includes(format)) {
+      return html.replace('(result)', '支持');
+    } else {
+      return html.replace('(result)', '不支持');
+    }
+  }).join('\n');
 })();
